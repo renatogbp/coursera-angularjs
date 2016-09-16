9c304f296ad09cf58c7a47c7c63994a800a7745e
Renatogbp <renatogbp@Renatos-MacBook-Pro.local>
2016-09-16 14:53:56 -0300

    last version

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,21 +1,37 @@
 (function () {
   'use strict';
 
-  angular.module('LunchCheck', [])
+  angular.module('LunchCheck',[])
   .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope']
 
   function LunchCheckController ($scope){
 
-    $scope.food = "something";
-    $scope.messageOutput = "something2"
+    $scope.foods = "";
+    $scope.messageOutput = "";
 
     $scope.displayMessage = function () {
-      var displayMessageOutput = "test";
-      $scope.messageOutput = "displayMessageOutput";
+      if($scope.foods){
+        var separator = ",";
+        var arrayOfStrings = $scope.foods.split(separator);
+        var count = 0;
+        for (var i=0; i<arrayOfStrings.length; i++){
+          if(!(arrayOfStrings[i].length == 0 || !arrayOfStrings[i].trim())){
+            count += 1;
+          }
+        }
+        if(count > 3 ){
+          $scope.messageOutput = "Too much!";
+        }else{
+          $scope.messageOutput = "Enjoy!";
+        }
+      }
+      else {
+        $scope.messageOutput = "Please enter data first";
+      }
     };
 
-  });
+  };
 
 })();
